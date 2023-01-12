c51b24961652a2d3af2874840115adfde2ccde8a
niloyabdullah <abdullahniloy149@gmail.com>
2023-01-12 20:39:05 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/', element: <div><h2>Default</h2></div>
+    },
+    {
+      path: '/home', element: <div><h2>Home</h2></div>
+    },
+    {
+      path: 'about', element: <div><h2>About</h2></div>
+    },
+
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}></RouterProvider>
+
     </div>
   );
 }
